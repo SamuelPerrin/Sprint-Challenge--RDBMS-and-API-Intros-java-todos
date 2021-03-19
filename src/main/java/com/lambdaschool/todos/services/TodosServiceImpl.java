a2ae224fe24a212a33bafbb77da27c43a7daeacc
SamuelPerrin <samwperrin@yahoo.com>
2021-03-19 09:58:16 -0500

    get PATCH working

--- src/main/java/com/lambdaschool/todos/services/TodosServiceImpl.java
+++ src/main/java/com/lambdaschool/todos/services/TodosServiceImpl.java
@@ -1,9 +1,12 @@
 package com.lambdaschool.todos.services;
 
+import com.lambdaschool.todos.models.Todos;
 import com.lambdaschool.todos.repository.TodosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
 
 @Transactional
 @Service(value = "todosService")
@@ -13,6 +16,9 @@ public class TodosServiceImpl implements TodosService {
 
     @Override
     public void markComplete(long todoid) {
-        
+        Todos newTodos = todosrepos.findById(todoid)
+                .orElseThrow(() -> new EntityNotFoundException("Todo " + todoid + " not found!"));
+
+        newTodos.setCompleted(true);
     }
 }
