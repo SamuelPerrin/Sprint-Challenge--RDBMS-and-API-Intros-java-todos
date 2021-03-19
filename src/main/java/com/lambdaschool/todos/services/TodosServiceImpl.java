7413eddeab193827c4ccb24de8fa49d3212c97a7
SamuelPerrin <samwperrin@yahoo.com>
2021-03-19 09:13:52 -0500

    set up project and write Todos model

---
+++ src/main/java/com/lambdaschool/todos/services/TodosServiceImpl.java
@@ -0,0 +1,18 @@
+package com.lambdaschool.todos.services;
+
+import com.lambdaschool.todos.repository.TodosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service(value = "todosService")
+public class TodosServiceImpl implements TodosService {
+    @Autowired
+    private TodosRepository todosrepos;
+
+    @Override
+    public void markComplete(long todoid) {
+        
+    }
+}
